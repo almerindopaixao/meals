c8712be2a1c6ae4020c203bf3cd694b075974cc7
Almerindo Paixão <almerindo_paixao@live.com>
2021-09-29 09:31:55 -0300

    Adicionando rotas nomeadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
+import './screens/categories_meals_screen.dart';
+
+import './utils/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
@@ -25,7 +28,11 @@ class MyApp extends StatelessWidget {
           )
         )
       ),
-      home: const CategoriesScreen(),
+      initialRoute: AppRoutes.HOME,
+      routes: {
+        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+      },
     );
   }
 }
